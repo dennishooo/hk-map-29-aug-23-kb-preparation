10f89502ea8d706b80b5f06c372f94794efa86d3
dennishooo <hkhdennis@gmail.com>
2023-10-21 12:18:31 +0800

    feat: add basic concept

---
+++ function.js
@@ -0,0 +1,18 @@
+function add(a, b) {
+    return a + b
+}
+
+const add2 = (a,b)=>{
+    return a + b
+}
+
+const add3 = (a,b) => a + b
+
+
+console.log(add(1,2))
+
+console.log(add2(1,2))
+
+console.log(add3(1,2))
+
+
